fcbfb1b58016be5381e3924b97e07588f5c4d6c1
Boris Lanovenko <blanovenko@gmail.com>
2023-06-08 11:26:01 +0300

    Solution task 1 [change]

--- HW_01/task01.java
+++ HW_01/task01.java
@@ -1,9 +1,9 @@
 package HW_01;
 import java.util.Scanner;
-
 /**
-  Вычислить сумма чисел от 1 до n
+ * Вычислить сумма чисел от 1 до n
  */
+
 public class task01 {
 
   public static void main(String[] args) {
@@ -13,8 +13,8 @@ public class task01 {
     int number = Integer.parseInt(numberString);
     System.out.println(number);
     int sum = 0;
-    for (int i=1; i<=number; i++){
-      sum+=i;
+    for (int i = 1; i <= number; i++) {
+      sum += i;
     }
     System.out.println("Sum of numbers from 1 to N = " + sum);
     iScanner.close();
