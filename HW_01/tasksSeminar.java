626bc22d1116ec13499cebaffdcb1b9800233da3
Boris Lanovenko <blanovenko@gmail.com>
2023-06-08 22:23:21 +0300

    Solutions tasks seminar 1

--- HW_01/tasksSeminar.java
+++ HW_01/tasksSeminar.java
@@ -1,14 +1,16 @@
 package HW_01;
-
+import java.io.*;
 public class tasksSeminar {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     task5();
     task8();
     task9();
 
   }
 
-  /*
+ 
+
+ /*
    * Во фразе "Добро пожаловать на курс по Java" 
    * переставить слова в обратном порядке
    */
@@ -62,8 +64,27 @@ public class tasksSeminar {
     else System.out.println("\nThis index is: " + result);
   }
 
+  /*
+  * Записать в файл следующие данные:
+  * Name Surname Age
+  * Kate Smith 20
+  * Paul Green 25
+  * Mike Black 23 
+  */
+  private static void task9() throws IOException {
+    File file = new File("Test.txt");
+    file.createNewFile();
+    FileWriter writer = new FileWriter(file);
+    writer.write(" Name, Surname, Age\n Kate Smith 20\n Paul Green 25\n Mike Black 23 ");
+    writer.flush();
+    writer.close();
 
-  private static void task9() {
-    
+    FileReader reader = new FileReader(file);
+    char[] text = new char[1000];
+    reader.read(text);
+    for (char c: text) {
+      System.out.print(c);
+    }
+    reader.close();
   }
 }
