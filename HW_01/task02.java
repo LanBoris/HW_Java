6f16cbb1f2ecce62e5a70751ebdd15c5a09160e1
Boris Lanovenko <blanovenko@gmail.com>
2023-06-08 11:26:23 +0300

    Solution task 2 [change]

--- HW_01/task02.java
+++ HW_01/task02.java
@@ -2,6 +2,28 @@ package HW_01;
 /*
  * Вывести все простые числа от 1 до 1000
  */
+
 public class task02 {
-  
+  public static void main(String[] args) {
+    for (int i = 0; i < 1000; i++) {
+      if (TestPrimeNumber(i)){
+        System.out.printf("%d ", i);
+      }
+    }
+  }
+
+  private static Boolean TestPrimeNumber(int num){
+    boolean result = true;
+    if (num <= 1) result = false;
+    for (int i=2; i<1000; i++){
+      if (num >= i && num%i == 0){
+        if (i!=num){
+          result = false;
+          break;
+        }
+      }
+    }
+    return result;
+  }
+
 }
